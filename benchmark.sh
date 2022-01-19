403c80b23e6d263c6114bd1a3276308004468c34
root <tim.darnell@hotmail.com>
2022-01-19 15:42:57 -0700

    Update benchmark script and FIO profiles

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,16 @@ STORAGECLASS=portworx-sc
 # Size of PVC used for FIO testing
 FIOSIZE=50Gi
 
+check_pvc () {
+PVCNUM=$(kubectl get pvc -A -o jsonpath='{.items[*].spec.storageClassName}' | grep $STORAGECLASS | wc -w)
+
+until [ "$PVCNUM" = "0" ]; do
+   echo "PVC found for StorageClass $STORAGECLASS. Waiting until existing PVC is removed/deleted."
+   sleep 10 
+   PVCNUM=$(kubectl get pvc -A -o jsonpath='{.items[*].spec.storageClassName}' | grep $STORAGECLASS | wc -w)
+done
+}
+
 fio_benchmark () {
 FILE=kubestr
 if [ -x "$FILE" ]; then
@@ -23,25 +33,39 @@ else
     exit 1
 fi
 
-echo "Running random RW mix FIO profile, this will take 15 minutes. System time is $(date)"
-./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/fio-rand-RW.fio -o json -e rand-RW-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+check_pvc
 
-echo "Running sequential RW mix FIO profile, this will take 15 minutes. System time is $(date)"
-./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/fio-seq-RW.fio -o json -e seq-RW-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+echo "Running random RW mix FIO profile. System time is $(date)"
+./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/px-rand-RW.fio -o json -e rand-RW-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
 
-echo "Running random read FIO profile, this will take 15 minutes. System time is $(date)"
-./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/fio-rand-read.fio -o json -e rand-read-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+check_pvc
 
-echo "Running random write FIO profile, this will take 15 minutes. System time is $(date)"
-./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/fio-rand-write.fio -o json -e rand-write-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+echo "Running sequential RW mix FIO profile. System time is $(date)"
+./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/px-seq-RW.fio -o json -e seq-RW-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
 
-echo "Running sequential read FIO profile, this will take 15 minutes. System time is $(date)"
-./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/fio-seq-read.fio -o json -e seq-read-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+check_pvc
 
-echo "Running sequential write FIO profile, this will take 15 minutes. System time is $(date)"
-./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/fio-seq-write.fio -o json -e seq-write-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+echo "Running random read FIO profile. System time is $(date)"
+./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/px-rand-read.fio -o json -e rand-read-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
 
-echo "All FIO tests completed. Please find results in files named *-$STORAGECLASS.json. Exiting."
+check_pvc
+
+echo "Running random write FIO profile. System time is $(date)"
+./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/px-rand-write.fio -o json -e rand-write-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+
+check_pvc
+
+echo "Running sequential read FIO profile. System time is $(date)"
+./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/px-seq-read.fio -o json -e seq-read-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+
+check_pvc
+
+echo "Running sequential write FIO profile. System time is $(date)"
+./kubestr fio -z $FIOSIZE -s $STORAGECLASS -f fio-profiles/px-seq-write.fio -o json -e seq-write-$STORAGECLASS-$(date '+%Y-%m-%d-%H%M%S').json >& /dev/null
+
+check_pvc
+
+echo "All FIO tests completed. Please find results in files named *-$STORAGECLASS.json."
 }
 
 pgbench () {
@@ -56,6 +80,8 @@ else
     echo "Could not find StorageClass $STORAGECLASS, please ensure you have created the StorageClass you wish to test and try again."
     exit 1
 fi
+
+check_pvc
 
 # Create the Postgres PVC
 cp postgres/postgres-pvc-template.yaml postgres/postgres-pvc.yaml
@@ -93,6 +119,7 @@ kubectl delete sts postgres
 kubectl delete all -l app=postgres
 kubectl delete cm postgres-config
 kubectl delete pvc postgres-data
+check_pvc
 }
 
 
